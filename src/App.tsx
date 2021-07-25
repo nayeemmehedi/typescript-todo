cc9c3a9d6199b4e029016f60915bb1cfa458df68
nayeemmehedi <boyneel79@gmail.com>
2021-07-25 22:58:24 +0600

    react typescript todo app

---
+++ src/App.tsx
@@ -0,0 +1,95 @@
+import React, { FC, ChangeEvent, useState } from "react"
+
+import "./App.css"
+import TodoTask from "./Components/TodoTask"
+ 
+import { ITask } from "./Interfaces";
+
+
+
+
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDealine] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDealine(Number(event.target.value));
+    }
+  };
+
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDealine(0);
+  };
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName != taskNameToDelete;
+      })
+    );
+  };
+
+
+
+
+
+  return (
+    <div className="App">
+
+    <h1>Please add ToDo:</h1>
+
+
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="name"
+            name="task"
+            value={task}
+            onChange={handleChange}
+          /> <br />
+          <input
+            type="number"
+            placeholder="DAY"
+            name="deadline"
+            value={deadline}
+            onChange={handleChange}
+          />
+        </div>
+        <br />
+       
+
+
+        
+        <button  onClick={addTask}>Click Here</button>
+      
+      </div>
+
+
+
+      <div className="todoList">
+
+
+
+        {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
+        })}
+
+      </div>
+
+
+
+    </div>
+
+  );
+};
+
+export default App;
